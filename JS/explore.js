00ec92802394eeed81bdc4f7339e8eb1659fbfd7
Wahed Koudsi <82679451+Wahed-koudsi2001@users.noreply.github.com>
2021-09-07 11:32:47 +0300

    Uploads

---
+++ JS/explore.js
@@ -0,0 +1,37 @@
+// Navigation Bar
+let muneIcon = document.querySelector('.mune-icon');
+let sidebar = document.querySelector('.sidebar');
+let container = document.querySelector('.container');
+
+muneIcon.onclick = function () {
+    sidebar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container');
+}
+
+// icons in navbar
+
+let show1 = document.getElementById('show1');
+let showOne = document.getElementById('showOne');
+let show2 = document.getElementById('show2');
+let showTwo = document.getElementById('showTwo');
+let show3 = document.getElementById('show3');
+let showThree = document.getElementById('showThree');
+
+show1.onclick = function () {
+    showOne.classList.toggle('show1-show');
+    showTwo.classList.remove('show2-show');
+    showThree.classList.remove('show3-show');
+}
+
+show2.onclick = function () {
+    showOne.classList.remove('show1-show');
+    showTwo.classList.toggle('show2-show');
+    showThree.classList.remove('show3-show');
+
+}
+show3.onclick = function () {
+    showOne.classList.remove('show1-show');
+    showTwo.classList.remove('show2-show');
+    showThree.classList.toggle('show3-show');
+
+}
